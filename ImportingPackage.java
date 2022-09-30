5ebe7cd5db147a475138e361c3d6c5c26c22236b
irtiza31 <91143203+irtiza31@users.noreply.github.com>
2022-09-30 20:23:53 +0530

    Add files via upload

---
+++ ImportingPackage.java
@@ -0,0 +1,15 @@
+package myPackage;
+
+import java.util.Scanner;
+
+public class ImportingPackage {
+
+	public static void main(String[] args) {
+
+		Scanner read = new Scanner(System.in);
+		
+		int i = read.nextInt();
+		
+		System.out.println("You have entered a number " + i);
+	}
+}
